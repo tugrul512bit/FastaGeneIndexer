1013b0c92f176bfccc988853de3d19ff7055aedf
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-02-25 17:39:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
-#include"examples/FastaGeneIndexer.h"
+#include"FastaGeneIndexer.h"
 
 int main(int argC, char** argV)
 {
-	std::string fileName = std::string(argV[argC-1]);
 	try
 	{
 		bool debug = true;
-		FastaGeneIndexer fasta(fileName,debug);
+		FastaGeneIndexer fasta("./data/influenza.fna",debug);
 		std::cout<<fasta.getDescriptor(0)<<std::endl;
-		std::cout<<fasta.getSequence(0)<<std::endl;		
+		std::cout<<fasta.getSequence(0)<<std::endl;	
+		
+		fasta.initDescriptorIndexMapping();
+		std::string sequence = fasta.getSequenceByDescriptor("gi|58576|gb|X52226|Influenza A virus (A/FPV/Rostock/34(H7N1)) gene for neuraminidase, genomic RNA");		
+		std::cout<< sequence <<std::endl;
 	}
 	catch (std::exception& ex)
 	{
